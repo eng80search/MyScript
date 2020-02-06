b2b92a32324b6cc242de721dc71b081f7e1ae61d
eng80search <eng80search@gmail.com>
2020-02-06 14:41:28 +0900

    PythonからExcelファイルを操作するスクリプトを追加

---
+++ Python/ExcelPaste.py
@@ -0,0 +1,119 @@
+import openpyxl
+
+wb = openpyxl.load_workbook(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\20200205_強制地図位置解除対象リスト_61-80.xlsx')
+#  wb = openpyxl.load_workbook(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\20200205_強制地図位置解除対象リスト_81-100.xlsx')
+#  wb = openpyxl.load_workbook(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\20200205_強制地図位置解除対象リスト_100-108.xlsx')
+
+#Debug用
+print(wb.sheetnames)
+
+ws1  = wb['1']
+ws2  = wb['2']
+ws3  = wb['3']
+ws4  = wb['4']
+ws5  = wb['5']
+ws6  = wb['6']
+ws7  = wb['7']
+ws8  = wb['8']
+ws9  = wb['9']
+ws10  = wb['10']
+ws11  = wb['11']
+ws12  = wb['12']
+ws13  = wb['13']
+ws14  = wb['14']
+ws15  = wb['15']
+ws16  = wb['16']
+ws17  = wb['17']
+ws18  = wb['18']
+ws19  = wb['19']
+ws20  = wb['20']
+
+
+img1  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\47605.PNG')
+img2  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\47734.PNG')
+img3  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\47758.PNG')
+img4  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48076.PNG')
+img5  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48178.PNG')
+img6  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48307.PNG')
+img7  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48478.PNG')
+img8  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48538.PNG')
+img9  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48568.PNG')
+img10 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48790.PNG')
+img11 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48823.PNG')
+img12 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\48937.PNG')
+img13 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\49024.PNG')
+img14 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\49039.PNG')
+img15 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\49036.PNG')
+img16 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\49069.PNG')
+img17 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\49279.PNG')
+img18 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\68995.PNG')
+img19 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\69178.PNG')
+img20 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_QA\69418.PNG')
+
+ws1.add_image( img1, 'A2' )
+ws2.add_image( img2, 'A2' )
+ws3.add_image( img3, 'A2' )
+ws4.add_image( img4, 'A2' )
+ws5.add_image( img5, 'A2' )
+ws6.add_image( img6, 'A2' )
+ws7.add_image( img7, 'A2' )
+ws8.add_image( img8, 'A2' )
+ws9.add_image( img9, 'A2' )
+ws10.add_image( img10, 'A2' )
+ws11.add_image( img11, 'A2' )
+ws12.add_image( img12, 'A2' )
+ws13.add_image( img13, 'A2' )
+ws14.add_image( img14, 'A2' )
+ws15.add_image( img15, 'A2' )
+ws16.add_image( img16, 'A2' )
+ws17.add_image( img17, 'A2' )
+ws18.add_image( img18, 'A2' )
+ws19.add_image( img19, 'A2' )
+ws20.add_image( img20, 'A2' )
+
+img1  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\47605.PNG')
+img2  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\47734.PNG')
+img3  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\47758.PNG')
+img4  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48076.PNG')
+img5  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48178.PNG')
+img6  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48307.PNG')
+img7  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48478.PNG')
+img8  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48538.PNG')
+img9  = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48568.PNG')
+img10 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48790.PNG')
+img11 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48823.PNG')
+img12 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\48937.PNG')
+img13 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\49024.PNG')
+img14 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\49039.PNG')
+img15 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\49036.PNG')
+img16 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\49069.PNG')
+img17 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\49279.PNG')
+img18 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\68995.PNG')
+img19 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\69178.PNG')
+img20 = openpyxl.drawing.image.Image(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\Image_Hon\69418.PNG')
+
+ws1.add_image( img1, 'A48' )
+ws2.add_image( img2, 'A48' )
+ws3.add_image( img3, 'A48' )
+ws4.add_image( img4, 'A48' )
+ws5.add_image( img5, 'A48' )
+ws6.add_image( img6, 'A48' )
+ws7.add_image( img7, 'A48' )
+ws8.add_image( img8, 'A48' )
+ws9.add_image( img9, 'A48' )
+ws10.add_image( img10, 'A48' )
+ws11.add_image( img11, 'A48' )
+ws12.add_image( img12, 'A48' )
+ws13.add_image( img13, 'A48' )
+ws14.add_image( img14, 'A48' )
+ws15.add_image( img15, 'A48' )
+ws16.add_image( img16, 'A48' )
+ws17.add_image( img17, 'A48' )
+ws18.add_image( img18, 'A48' )
+ws19.add_image( img19, 'A48' )
+ws20.add_image( img20, 'A48' )
+
+
+wb.save(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\20200205_強制地図位置解除対象リスト_61-80.xlsx')
+#  wb.save(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\20200205_強制地図位置解除対象リスト_81-100.xlsx')
+#  wb.save(r'C:\Users\setupuser\Documents\00_Ri\00_作業\70_定例作業\02_解除用\20200205_強制地図位置解除対象リスト_100-108.xlsx')
---
+++ Python/OpenUrl.py
@@ -0,0 +1,81 @@
+import webbrowser
+
+#  #  Stage指定URLを設定()
+#  url0  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69178"
+#  url0  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69178"
+
+#  url1  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=47605"
+#  url2  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=47734"
+#  url3  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=47758"
+#  url4  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48076"
+#  url5  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48178"
+#  url6  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48307"
+#  url7  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48478"
+#  url8  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48538"
+#  url9  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48568"
+#  url10 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48790"
+#  url11 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48823"
+#  url12 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=48937"
+#  url13 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=49024"
+#  url14 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=49039"
+#  url15 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=49036"
+#  url16 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=49069"
+#  url17 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=49279"
+#  url18 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=68995"
+#  url19 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69178"
+#  url20 = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69418"
+
+#  url1  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69655"
+#  url2  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69880"
+#  url3  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69940"
+#  url4  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=69964"
+#  url5  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=70363"
+#  url6  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=70375"
+#  url7  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=70936"
+#  url8  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=71284"
+#  url9  = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=71329"
+#  url10 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=71413"
+#  url11 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=71836"
+#  url12 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=71863"
+#  url13 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=71941"
+#  url14 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=72148"
+#  url15 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=72130"
+#  url16 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=72529"
+#  url17 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=72664"
+#  url18 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=72775"
+#  url19 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=73123"
+#  url20 = "https://acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=73426"
+
+url1  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=73543"
+url2  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=73939"
+url3  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=73957"
+url4  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=75154"
+url5  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=75169"
+url6  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=75598"
+url7  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=75847"
+url8  = "https://stage.acuvue.jnj.co.jp/store/detail.aspx?StoreSeq=75961"
+
+#  本番指定URLを設定
+
+#  指定URLを開く
+webbrowser.open(url1)
+webbrowser.open(url2)
+webbrowser.open(url3)
+webbrowser.open(url4)
+webbrowser.open(url5)
+webbrowser.open(url6)
+webbrowser.open(url7)
+webbrowser.open(url8)
+#  webbrowser.open(url9)
+#  webbrowser.open(url10)
+#  webbrowser.open(url11)
+#  webbrowser.open(url12)
+#  webbrowser.open(url13)
+#  webbrowser.open(url14)
+#  webbrowser.open(url15)
+#  webbrowser.open(url16)
+#  webbrowser.open(url17)
+#  webbrowser.open(url18)
+#  webbrowser.open(url19)
+#  webbrowser.open(url20)
+#
