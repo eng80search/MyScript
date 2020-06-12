ddb58c193ad1f23b1e6504aa1a07f0d6e6837973
eng80search <eng80search@gmail.com>
2020-06-12 11:54:04 +0900

    2020/06/12　Up

--- WindowsPowerShell/SimpleCommand.ps1
+++ WindowsPowerShell/SimpleCommand.ps1
@@ -1,4 +1,4 @@
 # ファイル一覧を取得してcsvファイルに出力するスクリプト
-$CONFIG_LIST_PATH = "C:\Users\setupuser\Documents\00_Ri\00_作業\00_Solace対応_20200210\05_リリース\20200227_Release_WEB03-04\Websites"
+$CONFIG_LIST_PATH = "C:\Users\setupuser\Documents\00_Ri\00_作業\00_sprint20_店舗詳細バナー表示\05_リリース\20200615_Release_WEB03-04\Websites"
 Set-Location -Path $CONFIG_LIST_PATH
 Get-ChildItem -Recurse * | Select-Object Name, FullName, LastWriteTime | Export-Csv -encoding default  filelist.csv
--- WindowsPowerShell/run.bat
+++ WindowsPowerShell/run.bat
@@ -2,7 +2,8 @@
 echo %date% %time% 
 echo 連続バッチ実行処理を開始します。。。
 
-call C:\Users\setupuser\Documents\00_Ri\01_Tool\11_myScript\WindowsPowerShell\DeleteFilesEscapeDll.ps1
+REM  call C:\Users\setupuser\Documents\00_Ri\01_Tool\11_myScript\WindowsPowerShell\DeleteFilesEscapeDll.ps1
+call C:\Users\setupuser\Documents\00_Ri\01_Tool\11_myScript\WindowsPowerShell\SimpleCommand.ps1
 REM  call D:\work\appconfig\DetailImport\2_DetailImport_gaiyo.bat
 REM  call D:\work\appconfig\DetailImport\3_DetailImport_meisai.bat
 REM  call D:\work\appconfig\DetailImport\4_DetailImport_Result.bat
