6b9a83cfcd411a714515e72872346d814d363b64
eng80search <eng80search@gmail.com>
2020-10-06 23:20:10 +0900

    modify ImageResizing python script to replace a part of the string in the template file

--- Python/ImageResizing.py
+++ Python/ImageResizing.py
@@ -20,6 +20,97 @@ import pandas as pd
 
 URL_PREFIX = "https://www.rakuten.ne.jp/gold/newimage/2015-7/"
 
+def resize_image(original_image_file, resized_image_file):
+    """画像を読み込んだ後、リサイズする
+
+    :original_image_file: TODO
+    :resized_image_file: TODO
+    :returns: TODO
+
+    """
+
+    #  画像ファイルをリサイズして保存
+    img = Image.open(original_image_file)
+    if i == 0:
+        img_resize = img.resize((900, 900))
+    else:
+        img_resize = img.resize((700, 700))
+    img_resize.save(resized_image_file)
+
+    return True
+
+def read_file(file_path):
+    """ファイルの内容を文字列として読み込む
+
+    :file_path: TODO
+    :returns: TODO
+
+    """
+
+    with open(file_path) as f:
+        file_content = f.read()
+    return file_content
+
+def write_file(file_path, pc_content):
+    """生成されたPC販売説明文をファイルに書き出す
+
+    :file_path: TODO
+    :returns: TODO
+
+    """
+
+    with open(file_path) as f:
+        file_content = f.read()
+    return file_content
+
+def read_template_list(pc_descript_1,pc_descript_2):
+    """PC販売説明文をテンプレートファイルからテンプレートリストを作成
+
+    :pc_descript_1: PC用販売説明文１
+    :pc_descript_2: PC用販売説明文２
+    :returns: 説明文リスト
+
+    """
+    pc_template_list = []
+
+    with open(pc_descript_1) as f1:
+        pc_template_list.append(f1.read())
+
+    with open(pc_descript_2) as f2:
+        pc_template_list.append(f2.read())
+
+    return pc_template_list
+
+def make_pc_descript(json_data, image_list):
+    """PC用販売説明文を作成する
+
+    :json_data: Json設定値
+    :image_list: 画像ファイル名リスト
+    """
+    pc_descript_1 = json_data["pc_descript_1"]
+    pc_descript_2 = json_data["pc_descript_2"]
+    pc_descript_out = json_data["pc_descript_out"]
+
+    pc_template_list = read_template_list(pc_descript_1, pc_descript_2)
+    pc_descript_str = ""
+
+    for i, image_name in enumerate(image_list):
+        print(image_name)
+        #  第一枚画像用
+        if i == 0:
+            pc_descript_str = "--------------第一枚画像説明文-----------------\r\n"
+            pc_descript_str += pc_template_list[0]
+            pc_descript_str = pc_descript_str.replace('$1', image_name)
+        #  第二枚画像以降用
+        else:
+            if i == 1:
+                pc_descript_str += "--------------第二枚以降画像説明文----------------\r\n"
+            pc_descript_str += pc_template_list[1]
+            pc_descript_str = pc_descript_str.replace('$1', image_name)
+
+    print(pc_descript_str)
+
+    return True
 
 def output_data(file_list):
     """TODO: item.csvファイルに必要なデータを作成
@@ -48,7 +139,6 @@ try:
         item_csv_temp_path = json_data["item_csv_temp_path"]
         log_file_path = json_data["log_file_path"]
         log_file_name = json_data["log_file_name"]
-        out_put_name = json_data["out_put_name"]
 
     log_file_full_path = os.path.join(log_file_path, log_file_name)
 
@@ -63,6 +153,9 @@ try:
     PATH = resize_folder
     os.chdir(PATH)
 
+    print("*resize_folder:" + resize_folder)
+    logging.info("*resize_folder:" + resize_folder)
+
 except Exception as e:
     print(e)
     logging.error(e)
@@ -72,7 +165,7 @@ except Exception as e:
 #  内包表記でパターンにマッチングするフォルダだけ処理(サブフォルダは対象外)
 dir_list = [p for p in glob.iglob("**") if re.search("([A-Z]\d{6})-(\d{3}$)", p)]
 #  print(type(dir_list))
-print(dir_list)
+#  print(dir_list)
 
 try:
     #  マッチングされたフォルダに対して処理を行う
@@ -80,8 +173,7 @@ try:
         #  ディレクトリ内でJPGファイルのみ取得
         #  windowsでありがちな*.jpgファイルを抽出の場合はPythonでは\.jpg$と書く
         original_image_files = [
-            p
-            for p in glob.iglob(image_dir + "/*")
+            p for p in glob.iglob(image_dir + "/*")
             if re.search("\.jpg$", p, re.IGNORECASE)
         ]
         #  image_files = [p for p in pathlib.Path(image_dir).iterdir() if re.search("\.jpg$", str(p))]
@@ -110,13 +202,8 @@ try:
             #  shutil.copy(original_image_file, resized_image_file)
 
             #  画像ファイルをリサイズして保存
-            img = Image.open(original_image_file)
-            if i == 0:
-                img_resize = img.resize((900, 900))
-            else:
-                img_resize = img.resize((700, 700))
-            img_resize.save(resized_image_file)
-
+            resize_image(original_image_file, resized_image_file)
+            #  オリジナル画像ファイルを移動する
             shutil.move(original_image_file, original_image_dir)
 
             image_list.append(resized_image_name)
@@ -139,6 +226,8 @@ try:
         df.to_csv(
             os.path.join(image_dir, "item.csv"), index=False, encoding="shift_jis"
         )
+        #  コピペー用のPC用販売説明文を生成する
+        make_pc_descript(json_data,image_list)
 
 except Exception as e:
     print(e)
