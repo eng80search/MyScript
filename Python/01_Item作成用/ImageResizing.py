25709c1eecfbb1df56ae617be144ab6fdf1426cb
eng80search <eng80search@gmail.com>
2020-10-25 23:28:52 +0900

    improve_pc description can be created automatically in ImageResizing.py

--- Python/01_Item作成用/ImageResizing.py
+++ Python/01_Item作成用/ImageResizing.py
@@ -69,21 +69,25 @@ def write_file(pc_descript_out, pc_descript_str):
     return True
 
 
-def read_template_list(pc_descript_1, pc_descript_2):
+def read_template_list(pc_descript_part1, pc_descript_part2, pc_descript_all):
     """PC販売説明文をテンプレートファイルからテンプレートリストを作成
 
-    :pc_descript_1: PC用販売説明文１
-    :pc_descript_2: PC用販売説明文２
+    :pc_descript_part1: PC用販売説明文第１枚画像用
+    :pc_descript_part2: PC用販売説明文第２枚画像以降用
+    :pc_descript_all: PC用販売説明文全文（part1とpart2合成したもの）
     :returns: 説明文リスト
 
     """
     pc_template_list = []
 
-    with open(pc_descript_1) as f1:
+    with open(pc_descript_part1) as f1:
         pc_template_list.append(f1.read())
 
-    with open(pc_descript_2) as f2:
+    with open(pc_descript_part2) as f2:
         pc_template_list.append(f2.read())
+
+    with open(pc_descript_all) as f3:
+        pc_template_list.append(f3.read())
 
     return pc_template_list
 
@@ -94,31 +98,40 @@ def make_pc_descript(json_data, image_list, image_dir):
     :json_data: Json設定値
     :image_list: 画像ファイル名リスト
     """
-    pc_descript_1 = json_data["pc_descript_1"]
-    pc_descript_2 = json_data["pc_descript_2"]
+    pc_descript_part1 = json_data["pc_descript_part1"]
+    pc_descript_part2 = json_data["pc_descript_part2"]
+    pc_descript_all = json_data["pc_descript_all"]
     pc_descript_out = json_data["pc_descript_out"]
 
-    pc_template_list = read_template_list(pc_descript_1, pc_descript_2)
-    pc_descript_str = ""
+    pc_template_list = read_template_list(pc_descript_part1,
+                                          pc_descript_part2, pc_descript_all)
+    descript_part1 = ""
+    descript_part2 = ""
+    descript_all = ""
 
     for i, image_name in enumerate(image_list):
         #  第一枚画像用
         if i == 0:
-            pc_descript_str = "--------------第一枚画像説明文-----------------\n"
-            pc_descript_str += pc_template_list[0]
-            pc_descript_str = pc_descript_str.replace("$1", image_name)
+            descript_part1 = "<!-- ----第一枚画像説明文----- -->\n"
+            descript_part1 += pc_template_list[0]
+            descript_part1 = descript_part1.replace("$1", image_name)
         #  第二枚画像以降用
         else:
             if i == 1:
-                pc_descript_str += "--------------第二枚以降画像説明文----------------\n"
-            pc_descript_str += pc_template_list[1]
-            pc_descript_str = pc_descript_str.replace("$1", image_name)
+                descript_part2 += "<!-- ----第二枚以降画像説明文----- -->\n"
+            descript_part2 += pc_template_list[1]
+            descript_part2 = descript_part2.replace("$1", image_name)
 
-    pc_descript_out = os.path.join(image_dir, pc_descript_out)
-    print(pc_descript_str)
-    write_file(pc_descript_out, pc_descript_str)
+    #  第一枚説明文と第二枚以降の説明文を統合して、PC説明文を仕上げる
+    descript_all = pc_template_list[2]
+    descript_all = descript_all.replace("$1", descript_part1)
+    descript_all = descript_all.replace("$2", descript_part2)
 
-    return True
+    #  pc_descript_out = os.path.join(image_dir, pc_descript_out)
+    #  print(descript_all)
+    #  write_file(pc_descript_out, descript_part)
+
+    return descript_all
 
 
 def make_image_url(file_list):
@@ -162,8 +175,9 @@ try:
     PATH = resize_folder
     os.chdir(PATH)
 
-    print("*resize_folder:" + resize_folder)
-    logging.info("*resize_folder:" + resize_folder)
+    log_info = "*resize_root folder -> {0}".format(resize_folder) 
+    print(log_info)
+    logging.info(log_info)
 
 except Exception as e:
     print(e)
@@ -186,10 +200,10 @@ try:
             for p in glob.iglob(image_dir + "/*")
             if re.search("\.jpg$", p, re.IGNORECASE)
         ]
-        #  image_files = [p for p in pathlib.Path(image_dir).iterdir() if re.search("\.jpg$", str(p))]
 
-        #  print(image_files)
-        #  logging.info(image_files)
+        log_info = "--image resize_dir is {0}".format(image_dir)
+        print(log_info)
+        logging.info(log_info)
 
         #  元画像を保存するフォルダを作成
         original_image_dir = image_dir + "\\00_original"
@@ -230,7 +244,7 @@ try:
 
             image_list.append(resized_image_name)
 
-            log_info = "**original:{0}->resized:{1}".format(
+            log_info = "  resized:from [{0}] -> to [{1}]".format(
                 original_image_file, resized_image_file
             )
             print(log_info)
@@ -243,14 +257,41 @@ try:
             out_image_url = make_image_url(image_list)
             #  print(out_image_url)
 
+            #  コピペー用のPC用販売説明文を生成する
+            pc_descript_all = make_pc_descript(json_data, image_list,
+                                               resized_image_dir)
+
             #  item.csvテンプレートを読み込む
             df = pd.read_csv(item_csv_temp_path, encoding="shift_jis")
+
+            pc_descript_item = df.iat[0, 27]
+
+            #  pythonの正規表現では途中から一部の文字列を抽出する際には、その部分を括弧で囲み、
+            #  group(1)でその部分を取得する。group(0)は全体を取得する
+            #  複数の行を跨って検索する際には\s:空白文字 \S:空白以外の文字 *?:繰り返しが最小限マッチ
+            patternReplace = r'[\s\S]*?(<table\s+width="700"\s+border="0"\s+align="center">[\s\S]*?</table>)' 
+            #  正規表現確認用コード
+            #  m = re.match(patternReplace, pc_descript_item)
+            #  print(m.group(1))
+
+            #  正規表現でマッチングして、その対象を置換する場合はre.sub関すを使用する
+            pc_descript_item = re.sub(patternReplace, pc_descript_all, pc_descript_item)
+            #  print(pc_descript_item)
+            #  商品画像URLを設定
+            df.iat[0, 27] = pc_descript_item
+
             #  商品画像URLを設定
             df.iat[0, 28] = out_image_url
 
-            df.to_csv(os.path.join(resized_image_dir, "item.csv"), index=False, encoding="shift_jis")
-            #  コピペー用のPC用販売説明文を生成する
-            make_pc_descript(json_data, image_list, resized_image_dir)
+            df.to_csv(os.path.join(resized_image_dir, "item.csv"), 
+                      index=False, encoding="shift_jis")
+
+        #  リスサイズ対象が存在しない
+        else:
+
+            log_info = "     [Not exists target Files]"
+            print(log_info)
+            logging.info(log_info)
 
 except Exception as e:
     print(e)
