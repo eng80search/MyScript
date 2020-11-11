47d95fc9a1c767c82e389e3c825a8084416ab730
eng80search <eng80search@gmail.com>
2020-11-11 17:47:20 +0900

    Added_ MariaDB 強制地図変更_作業用 sql script

---
+++ MariaDB/強制地図変更_作業用.sql
@@ -0,0 +1,9 @@
+-- 地図位置変更作業用クエリ
+-- 前回解除しなかった店舗の備考欄に「前回解除せず」をつける
+SELECT a.no, a.clinic_id
+       ,(CASE WHEN b.clinic_id IS null THEN '' ELSE '前回解除せず' END)  '備考'
+		 , b.clinic_id
+FROM temp_new a
+LEFT JOIN temp_old b
+ON a.clinic_id = b.clinic_id 
+ORDER BY a.no asc
