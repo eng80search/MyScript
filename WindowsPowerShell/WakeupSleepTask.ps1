3a717e39fd2b852ed3f3980ca3a153c2cac11b5a
eng80search <eng80search@gmail.com>
2021-07-02 20:47:51 +0900

    Added_windowsPowerShell folder

---
+++ WindowsPowerShell/WakeupSleepTask.ps1
@@ -0,0 +1,19 @@
+﻿$script = '$signature = @"
+[DllImport("powrprof.dll")]
+public static extern bool SetSuspendState(bool Hibernate,bool ForceCritical,bool DisableWakeEvent);
+"@
+$func = Add-Type -memberDefinition $signature -namespace "Win32Functions" -name "SetSuspendStateFunction" -passThru
+$func::SetSuspendState($false,$true,$false)'
+
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($script)
+$encodedCommand = [Convert]::ToBase64String($bytes)
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-encodedCommand $encodedCommand"
+
+$trigger = New-ScheduledTaskTrigger -Daily -DaysInterval 1 -At 08:09
+Register-ScheduledTask -TaskName "Sleep" -Action $action -Trigger $trigger -Force
+
+$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c echo hello"
+$settings = New-ScheduledTaskSettingsSet -WakeToRun
+$trigger = New-ScheduledTaskTrigger -Daily -DaysInterval 1 -At 08:12
+Register-ScheduledTask -TaskName "WakeUp" -Action $action -Settings $settings -Trigger $trigger -Force
