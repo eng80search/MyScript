1c7e15182ec81607fb0404526b0fa822a2f93db2
eng80search <eng80search@gmail.com>
2021-09-20 16:18:25 +0900

    Added_ZaikoItemFileCopy module in python

---
+++ Python/01_ItemCreate/ZaikoItemFileCopy.py
@@ -0,0 +1,150 @@
+import glob
+import json
+import logging
+import os
+import re
+import sys
+import traceback
+
+import pandas as pd
+
+
+#  自分より上のフォルダにあるモジュールを参照するために必要な設定
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+import my_lib
+
+#  仕様： 在庫ロボット用ファイルは指定のフォルダから目的フォルダへコピー
+#         対象のコピー元フォルダ： (例) A210822-213
+#         対象のファイル： item.csv select.csv
+
+#         コピー先フォルダ： コピー元フォルダ名と同じ
+#         対象のファイル： item-213.csv select-213.csv
+#         select-213.csvファイルは以下の2列が追加される
+#           1.[在庫あり時納期管理番号]
+#            値の決め方：
+#                  IF [項目選択肢別在庫用在庫数]=0:   Value = 1000
+#                  IF [項目選択肢別在庫用在庫数]=100: Value = 1
+#           2.[在庫切れ時納期管理番号]
+#            値の決め方：
+#                  [値なし]
+
+#  他に考慮すべき点
+#  1. エラーはログに吐く
+
+
+def add_column_to_select_file(csv_file):
+    """select.csvファイルに列を追加
+
+    :csv_file: TODO
+    :returns: TODO
+
+    """
+
+    #  windows系のcsvファイルを読み込む際にはencodingを指定すべき
+    df = pd.read_csv(csv_file, encoding='cp932')
+    #  新規列を追加、既存の列の値から新規列の値を設定する
+    #  強力な機能！一行ですべて行に対して処理を行う
+    df['在庫あり時納期管理番号'] = df['項目選択肢別在庫用在庫数'].apply(
+                                    lambda x: 1000 if x == 0 else 1)
+    df['在庫切れ時納期管理番号'] = ''
+
+    #  既存のselect.csvを上書きする mode='w' 意味はなければ新規作成、あれば上書き
+    df.to_csv(csv_file, mode='w', encoding='cp932')
+
+def zaiko_item_file_copy():
+    """在庫ロボット登録用ファイル2つをテンプレートフォルダからコピーし、
+       select.csvファイルは2列追加する
+       item.csvファイルはそのままコピー
+
+    :arg1: 
+    :returns: 
+
+    """
+    try:
+
+        _fileio = my_lib.FileIo()
+
+        #  設定ファイルから各パラメータを取得する
+        with open(
+            "settings_zaikoItemFileCopy.json", "r", encoding="utf-8"
+        ) as json_file:
+            json_data = json.loads(json_file.read())
+
+            zaiko_csv_temp_root_path = json_data["zaiko_csv_temp_root_path"]
+            zaiko_csv_dest_root_path = json_data["zaiko_csv_dest_root_path"]
+            log_file_path = json_data["log_file_path"]
+            log_file_name = json_data["log_file_name"]
+
+        log_file_full_path = os.path.join(log_file_path, log_file_name)
+
+        #  ログ設定
+        formatter = "%(levelname)s : %(asctime)s : %(message)s"
+        logging.basicConfig(
+            filename=log_file_full_path, level=logging.INFO, format=formatter
+        )
+
+        #  コピー元フォルダからテンプレートファイルをコピーする
+        #  カレントディレクトリをコピー元フォルダに変更
+        PATH = zaiko_csv_temp_root_path
+        os.chdir(PATH)
+
+        log_info = "## zaiko_source_dir_root : {0}".format(PATH)
+        print(log_info)
+        logging.info(log_info)
+
+        #  内包表記でパターンにマッチングするフォルダだけ処理(サブフォルダは対象外)
+        dir_list = [
+            p
+            for p in glob.iglob("**")
+            if re.search("(^[A-Z]\d{6})-(\d{3})(-([A-Z]{2}))?$", p)
+        ]
+
+        for zaiko_source_dir in dir_list:
+            #  glob関数は特定のパターンにマッチするファイルを取得
+            #  zaiko_source_dir + "/*"の意味はzaiko_source_dirの直下のファイルリスト
+            zaiko_soure_files = [
+                p
+                for p in glob.iglob(zaiko_source_dir + "/*")
+                if re.search(
+                    r"(^item\.csv)|(^select\.csv)", os.path.basename(p), re.IGNORECASE) ]
+
+            log_info = "** zaiko_source dir: {0} ".format(zaiko_source_dir)
+            print(log_info)
+            logging.info(log_info)
+
+            #  コピー先のフォルダを作成する(既にあればそのまま)
+            zaiko_csv_dest_path = _fileio.combine_file_path(
+                zaiko_csv_dest_root_path, zaiko_source_dir)
+            _fileio.make_dirs(zaiko_csv_dest_path)
+
+            #  split("-",1)は一回のみ分割するとの意味 A12-01-Aは["A12", "01-A"]
+            dir_suffix = zaiko_source_dir.split("-", 1)[1]
+
+            for i, zaiko_source_file in enumerate(zaiko_soure_files):
+
+                zaiko_dest_file = _fileio.get_filename_without_extention(zaiko_source_file)
+                #  ファイル名を変更する
+                zaiko_dest_file += "-" + dir_suffix + ".csv"
+                zaiko_dest_file = _fileio.combine_file_path(zaiko_csv_dest_path, zaiko_dest_file)
+
+                log_info = "     --copy from [{0}] -> to [{1}]".format(
+                        zaiko_source_file, zaiko_dest_file)
+                print(log_info)
+                logging.info(log_info)
+
+                #  例：在庫ロボットフォルダ名 : A210822-212の場合
+                #  select.csv -> select-212.csv(ファイルに新たに２列追加)
+                #  item.csv -> item-212.csv(ファイルコピーのみ)
+                _fileio.copy_file(zaiko_source_file, zaiko_dest_file)
+
+                if zaiko_source_file == "select.csv":
+                    #  selectファイルの列を追加する
+                    add_column_to_select_file(zaiko_dest_file)
+
+    except Exception as e:
+        print(traceback.format_exc())
+        logging.error(traceback.format_exc())
+
+
+if __name__ == "__main__":
+    zaiko_item_file_copy()
--- Python/my_lib/lib_file.py
+++ Python/my_lib/lib_file.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import shutil
+import pathlib
 
 
 class FileIo:
@@ -40,3 +42,91 @@ class FileIo:
         path = os.path.join(directory, file_name)
 
         return path
+
+    def make_dirs(self, dir_path):
+        """深い階層のディレクトリまで再帰的にフォルダを作成する
+           注：すでに存在する際には作成しない
+        例：C:\\00_dir\\01_sub_dir\\を一気に作成
+
+        :dir_path: TODO
+        :returns: TODO
+
+        """
+        #  ディレクトリが存在しない場合のみ、作成する
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+
+    def delete_dirs(self, dir_path):
+        """深い改装のディレクトリまで一気に削除する
+
+        :dir_path: TODO
+        :returns: TODO
+
+        """
+        #  ディレクトリが存在する場合のみ、削除する
+        if os.path.isdir(dir_path):
+            shutil.rmtree(dir_path)
+
+    def get_directory_name_from_file(self, file_name_with_path):
+        """フォルダパス付きファイル名からフォルダ名を取得する
+
+        :file_name_with_path: C:\\dir1\\dir2\\test.txt
+        :returns: C:\\dir1\\dir2
+
+        """
+        p_file = pathlib.Path(file_name_with_path)
+        #  ファイルが所在しているディレクトリ名を取得
+        current_dir = p_file.parent
+
+        return current_dir
+
+    def get_filename_without_extention(self, file_name):
+        """拡張子のないファイル名返す（a.txt -> a）
+
+        :file_name: ファイル名のみもしくはフォルダパス付き
+        :returns: TODO
+
+        """
+        file_name = os.path.basename(file_name)
+        file_name_without_extention = os.path.splitext(file_name)[0]
+        return file_name_without_extention
+
+    def get_file_extention(self, file_name):
+        """ファイルの拡張子を返す（a.txt -> .txt）
+
+        :file_name: ファイル名のみもしくはフォルダパス付き
+        :returns: 拡張子
+
+        """
+        file_name = os.path.basename(file_name)
+        file_name_without_extention = os.path.splitext(file_name)[1]
+        return file_name_without_extention
+
+    def copy_file(self, source_file, dest_file):
+        """ファイルをコピーする。既にファイルが存在する場合は上書きされる
+           コピー先のディレクトリがなければエラーになる
+
+        :source_file: C:\\dir1\\dir2\\source.txt
+        :dest_file: D:\\dir1\\dest.txt
+        :returns: TODO
+
+        """
+
+        #  ファイルをコピー
+        shutil.copy2(source_file, dest_file)
+
+    def copy_file_with_check_path(self, source_file, dest_file):
+        """ファイルをコピーする。
+           コピー先のディレクトリをチェックし、なければ作成する
+
+        :source_file: C:\\dir1\\dir2\\source.txt
+        :dest_file: D:\\dir1\\dest.txt
+        :returns: TODO
+
+        """
+        dest_dir = self.get_directory_name_from_file(dest_file)
+        #  コピー先のフォルダがなければ、先に作成する
+        self.make_dirs(dest_dir)
+
+        #  ファイルをコピー
+        shutil.copy2(source_file, dest_file)
